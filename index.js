0f3ec2878b07b7a0d15f87b123dbfb1b33392aa9
Jillu Rahman Jibon <jillurahmanjibon@gmail.com>
2022-10-24 12:26:47 +0600

    server update

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 2700;
 
-const categoris = require("./data/categories.json");
-const news = require("./data/news.json");
+app.use(cors());
+app.listen(port, () => {
+	console.log("port is running: ", { port });
+});
 app.get("/", (req, res) => {
-	res.send("news portal running");
+	res.send("Now ready for use");
+});
+const categories = require("./data/categories.json");
+app.get("/news_categories", (req, res) => {
+	res.send(categories);
 });
 
-app.get("/news-categories", (req, res) => {
-	res.send(categoris);
+const news = require("./data/news.json");
+app.get("/category/:id", (req, res) => {
+	const id = req.params.id;
+	if (id === "08") {
+		res.send(news);
+	} else {
+		const selectCategories = news.filter(n => n.category_id === id);
+		res.send(selectCategories);
+	}
+});
+app.get("/news/:id", (req, res) => {
+	const id = req.params.id;
+	const selectNews = news.find(n => n._id === id);
+	res.send(selectNews);
 });
 app.get("/news", (req, res) => {
 	res.send(news);
-});
-app.use(cors());
-app.listen(port, () => {
-	console.log("port is running now ", { port });
 });
